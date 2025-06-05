8bc6d01b40e659b3f860abd4114e19db6d0d9e3d
Nickolas <eagle.dev.stack@gmail.com>
2025-06-05 15:02:50 +0400

    Добавление функции проверки соединения с базой данных

---
+++ funcs/db.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+)
+
+var host string = os.Getenv("POSTGRES_HOST")
+
+var port string = os.Getenv("POSTGRES_PORT")
+
+var user string = os.Getenv("POSTGRES_USER")
+
+var password string = os.Getenv("POSTGRES_PASSWORD")
+
+var dbname string = os.Getenv("POSTGRES_DB")
+
+var psqlInfo string = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+func CheckConnection() {
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+
+		log.Fatal("Error opening connection to database")
+
+		return
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+
+	if err != nil {
+
+		log.Fatal("Error ping to database")
+
+		return
+	}
+
+	slog.Info("Successfully connected!")
+
+}
