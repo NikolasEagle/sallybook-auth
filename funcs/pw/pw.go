b27179a0bd01f816106eb1bc1ff37f8c1fc5a386
Nickolas <eagle.dev.stack@gmail.com>
2025-06-06 13:33:42 +0400

    Создание функции хеширования пароля

---
+++ funcs/pw/pw.go
@@ -0,0 +1,11 @@
+package pw
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 15)
+
+	return string(bytes), err
+
+}
