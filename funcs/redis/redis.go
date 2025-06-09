5cf57a28ba828d848a0b7222de09e3b0e9045519
Nickolas <eagle.dev.stack@gmail.com>
2025-06-09 11:02:58 +0400

    Инициализация данных redis-хранилища

---
+++ funcs/redis/redis.go
@@ -0,0 +1,38 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sallybook-auth/funcs/convert"
+
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis"
+)
+
+var _ = godotenv.Load()
+
+var (
+	host string = os.Getenv("REDIS_HOST")
+
+	port string = os.Getenv("REDIS_PORT")
+
+	password string = os.Getenv("REDIS_PASSWORD")
+
+	db int = convert.GetEnvAsInt(os.Getenv("REDIS_DB"))
+)
+
+var ctx = context.Background()
+
+func CheckConnection() {
+
+	client := redis.NewClient(&redis.Options{
+
+		Addr: fmt.Sprintf("%s:%s", host, port),
+
+		Password: password,
+
+		DB: db,
+	})
+
+}
