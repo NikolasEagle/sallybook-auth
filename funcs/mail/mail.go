2554b278085676b1802ceb305077c9236854a7dd
Nickolas <eagle.dev.stack@gmail.com>
2025-06-10 09:32:19 +0400

    Начало реализации функции SendMessageToAdmin

--- funcs/mail/mail.go
+++ funcs/mail/mail.go
@@ -1,9 +1,11 @@
 package mail
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
 )
 
 var _ = godotenv.Load()
@@ -19,3 +21,25 @@ var (
 
 	smtpRecipient = os.Getenv("SMTP_RECIPIENT")
 )
+
+func SendMessageToAdmin(first_name, second_name, email, password string) {
+
+	message := gomail.NewMessage()
+
+	message.SetHeader("From", fmt.Sprintf("MouseBook <%s>", smtpUser))
+
+	message.SetHeader("To", smtpRecipient)
+
+	message.SetHeader("Subject", "Зарегистрирован новый пользователь")
+
+	message.SetBody("text/html", fmt.Sprintf(`
+	
+		<h1>NEW USER</h1>
+        <p>Name: %s</p>
+        <p>Surname: %s</p>
+        <p><b>Email:%s</p>
+        <p><b>Password:</b> %s</p>
+	
+	`, first_name, second_name, email, password))
+
+}
